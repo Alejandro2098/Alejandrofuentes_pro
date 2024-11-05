5c7ad839fe8f13035e6b943566f04175f3f0a0da
Alejandro2098 <fuenmarti11@gmail.com>
2024-11-05 09:58:54 +0100

    actualizar
    actualizar

--- EXAM 2 ALEJANDRO FUENTES/examen2resulelto/src/examen2.java
+++ EXAM 2 ALEJANDRO FUENTES/examen2resulelto/src/examen2.java
@@ -1,10 +1,40 @@
 
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class examen2{
+public class examen2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Elige el ejercicio a ejecutar:");
+            System.out.println("1. Operaciones matemáticas");
+            System.out.println("2. Caja fuerte");
+            System.out.println("3. Conversión de base");
+            System.out.println("4. Salir");
+
+            int opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    ejercicio1(scanner);
+                    break;
+                case 2:
+                    ejercicio3(scanner);
+                    break;
+                case 3:
+                    ejercicio4(scanner);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, selecciona una opción válida.");
+            }
+        }
+    }
+
+    public static void ejercicio1(Scanner scanner) {
         while (true) {
             System.out.print("Introduce el primer número: ");
             double numero1 = scanner.nextDouble();
@@ -53,84 +83,68 @@ public class examen2{
         }
     }
 
+    public static void ejercicio3(Scanner scanner) {
+        final int claveCorrecta = 1234;  // Clave de apertura de la caja fuerte
+        int intentos = 4;  // Número de intentos permitidos
 
+        System.out.println("Bienvenido al sistema de apertura de la caja fuerte.");
 
-    public class CajaFuerte {
-        public static void main(String[] args) {
-            final int claveCorrecta = 1234;  // Clave de apertura de la caja fuerte
-            int intentos = 4;  // Número de intentos permitidos
-            Scanner scanner = new Scanner(System.in);
+        while (intentos > 0) {
+            System.out.print("Introduce la clave de 4 dígitos: ");
+            int claveUsuario = scanner.nextInt();
 
-            System.out.println("Bienvenido al sistema de apertura de la caja fuerte.");
-
-            while (intentos > 0) {
-                System.out.print("Introduce la clave de 4 dígitos: ");
-                int claveUsuario = scanner.nextInt();
-
-                if (claveUsuario == claveCorrecta) {
-                    System.out.println("Perfecto, la caja ha sido abierta");
-                    break;
+            if (claveUsuario == claveCorrecta) {
+                System.out.println("Perfecto, la caja ha sido abierta");
+                break;
+            } else {
+                intentos--;
+                if (intentos > 0) {
+                    System.out.println("Intento fallido, prueba de nuevo. Te quedan " + intentos + " intentos.");
                 } else {
-                    intentos--;
-                    if (intentos > 0) {
-                        System.out.println("Intento fallido, prueba de nuevo. Te quedan " + intentos + " intentos.");
+                    System.out.println("Lo siento, caja bloqueada.");
+                }
+            }
+        }
+    }
+
+    public static void ejercicio4(Scanner scanner) {
+        while (true) {
+            System.out.print("Introduce un número en base decimal: ");
+            int numeroDecimal = scanner.nextInt();
+
+            if (numeroDecimal < 0) {
+                System.out.println("No se puede hacer la operación con números negativos.");
+            } else {
+                while (true) {
+                    System.out.println("Elige el tipo de cambio de base:");
+                    System.out.println("a. Base 2");
+                    System.out.println("b. Base 8");
+                    System.out.println("c. Base 16");
+                    System.out.println("d. Salir");
+
+                    char opcion = scanner.next().charAt(0);
+
+                    if (opcion == 'a') {
+                        System.out.println("El número en base 2 es: " + Integer.toBinaryString(numeroDecimal));
+                    } else if (opcion == 'b') {
+                        System.out.println("El número en base 8 es: " + Integer.toOctalString(numeroDecimal));
+                    } else if (opcion == 'c') {
+                        System.out.println("El número en base 16 es: " + Integer.toHexString(numeroDecimal).toUpperCase());
+                    } else if (opcion == 'd') {
+                        System.out.println("Saliendo del menú.");
+                        break;
                     } else {
-                        System.out.println("Lo siento, caja bloqueada.");
+                        System.out.println("Opción no válida. Por favor, selecciona una opción válida.");
                     }
                 }
             }
 
-            scanner.close();
-        }
-    }
-
-
-
-    public class ConversionBase {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-
-            while (true) {
-                System.out.print("Introduce un número en base decimal: ");
-                int numeroDecimal = scanner.nextInt();
-
-                if (numeroDecimal < 0) {
-                    System.out.println("No se puede hacer la operación con números negativos.");
-                } else {
-                    while (true) {
-                        System.out.println("Elige el tipo de cambio de base:");
-                        System.out.println("a. Base 2");
-                        System.out.println("b. Base 8");
-                        System.out.println("c. Base 16");
-                        System.out.println("d. Salir");
-
-                        char opcion = scanner.next().charAt(0);
-
-                        if (opcion == 'a') {
-                            System.out.println("El número en base 2 es: " + Integer.toBinaryString(numeroDecimal));
-                        } else if (opcion == 'b') {
-                            System.out.println("El número en base 8 es: " + Integer.toOctalString(numeroDecimal));
-                        } else if (opcion == 'c') {
-                            System.out.println("El número en base 16 es: " + Integer.toHexString(numeroDecimal).toUpperCase());
-                        } else if (opcion == 'd') {
-                            System.out.println("Saliendo del menú.");
-                            break;
-                        } else {
-                            System.out.println("Opción no válida. Por favor, selecciona una opción válida.");
-                        }
-                    }
-                }
-
-                System.out.print("¿Deseas introducir otro número? (s/n): ");
-                char continuar = scanner.next().charAt(0);
-                if (continuar == 'n') {
-                    System.out.println("Adiós.");
-                    break;
-                }
+            System.out.print("¿Deseas introducir otro número? (s/n): ");
+            char continuar = scanner.next().charAt(0);
+            if (continuar == 'n') {
+                System.out.println("Adiós.");
+                break;
             }
-
-            scanner.close();
         }
     }
-
 }
