4175802e0edfaabf685fb62172c23a73f1d27707
Alejandro2098 <fuenmarti11@gmail.com>
2025-09-12 22:55:35 +0200

    update

---
+++ 1º trimestre 2/repaso1curso/src/Entrada.java
@@ -0,0 +1,25 @@
+public class Entrada {
+
+public static void main (String[]args ){
+
+    System.out.println("Primer programa de repaso");
+    //variable-> guardar valoe que sera utilizado
+    final String DNI="12345A;"
+    String nombre="alejandro";
+    String correo;//null
+    int edad=26;
+    System.out.print("Mi nombre es %s y ");
+
+
+
+    //Array --> lista dno dinamica -->final
+    //Arraylist/ HashTable -> lista dinamica-> final
+
+
+
+
+}
+
+
+
+}
